082f791adacdddb4f69c36e40a2c726d87d96b94
“Rutendo <rlkuuzabuwe@icloud.com>
2024-03-09 16:40:14 +0000

    updated-scores.js-file

--- assets/javascript/scores.js
+++ assets/javascript/scores.js
@@ -1,24 +1,14 @@
-//* Retrieve user initials and scores from localStorage *\\
-
-const userInitials = localStorgage.getItem("initial");
-const userScores = localStorage.getItem("socre");
-
-//* Get reference to the clear button and the highscore list element *\\
-
+const userInitials = localStorage.getItem("initial");
+const userScores = localStorage.getItem("score");
 const clearBtn = document.getElementById("clear");
 const highScoreListElement = document.getElementById("highscores");
 
-//* Display user initials and scores *\\
-
 if (userInitials && userScores) {
-    const listItem = document.createElement ("li");
-    listItem.textContent = '${userInitials} - ${userScores}';
-    highScoreListElement.appendChild (listItem);
+    highScoreListElement.innerHTML += `<li>${userInitials} - ${userScores}</li>`;
 }
 
-//* Remove highscore when clear button is clicked *\\
-
-clearBtn.addEventListender("click", function () {
+clearBtn.addEventListener("click", () => {
     highScoreListElement.innerHTML = "";
 });
+
 
