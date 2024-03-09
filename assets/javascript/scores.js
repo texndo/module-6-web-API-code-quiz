f2f8ea2c2b5a4d83feb2eb628a9198468754cb74
“Rutendo <rlkuuzabuwe@icloud.com>
2024-03-09 22:02:33 +0000

    fixed-saving-highscores-issue-attempt

--- assets/javascript/scores.js
+++ assets/javascript/scores.js
@@ -4,11 +4,24 @@ const clearBtn = document.getElementById("clear");
 const highScoreListElement = document.getElementById("highscores");
 
 if (userInitials && userScores) {
-    highScoreListElement.innerHTML += `<li>${userInitials} - ${userScores}</li>`;
+    highScoreListElement.innerHTML += '<li>${userInitials} - ${userScores}</li>';
 }
 
 clearBtn.addEventListener("click", () => {
     highScoreListElement.innerHTML = "";
 });
 
+function saveHighscore(initials, score) {
+    const existingInitials = localStorage.getItem("initials");
+    const existingScores = local.Storage.getItem("scores");
+
+    let initialsArray = existingInitials ? JSON.parse(existingInitials) : [];
+    let scoresArray = existingScores ? JSON.parse(existingScores) : [];
+
+    initialsArray.push(initials);
+    scoresArray.push(score);
+
+    localStorage.setItem("initials", JSON.stringify(initialsArray));
+    localStorage.setItem("scores", JSON.stringify(scoresArray));
+}
 
